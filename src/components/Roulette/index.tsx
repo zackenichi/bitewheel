20e7a35dc04b0080ce747aeaef3ecbb17a013e0f
Marcris Maligmat <marcmaligmat@gmail.com>
2024-08-21 09:00:44 +0800

    Fix on wrong copy of roulette component code

--- src/components/Roulette/index.tsx
+++ src/components/Roulette/index.tsx
@@ -1,121 +1,122 @@
-'use client';
+import { Button, Container, Grid } from '@mui/material';
+import { FC, useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+import { useSelector, useDispatch } from 'react-redux';
 
-import dynamic from 'next/dynamic';
-
-import React, { Fragment, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Container, Grid, Typography } from '@mui/material';
-
-import { setRestaurants } from '@/store/restaurant';
+// import { setNewWinner, setShowWinner } from '../../features/winner';
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
 import { RootState } from '@/store';
+// import { removeEmptyEntries } from '../../features/entry';
+import confetti from 'canvas-confetti';
+import { setSelectedRestaurant, setShowRestaurant } from '@/store/restaurant';
 
-const SearchBar = dynamic(() => import('@/components/Search/SearchBar'), {
-  ssr: false,
-});
-const Restaurants = dynamic(() => import('@/components/Restaurants'), {
-  ssr: false,
-});
-const Roulette = dynamic(() => import('@/components/Roulette'), { ssr: false });
-const EmptyList = dynamic(
-  () => import('@/components/UI').then((mod) => mod.EmptyList),
-  {
-    ssr: false,
+const truncateString = (str: string, num: number) => {
+  if (str.length <= num) {
+    return str;
   }
-);
-const LinearBuffer = dynamic(
-  () => import('@/components/UI').then((mod) => mod.LinearBuffer),
-  {
-    ssr: false,
-  }
-);
-const SelectedRestaurant = dynamic(
-  () =>
-    import('@/components/SelectedRestaurant').then(
-      (mod) => mod.SelectedRestaurant
-    ),
-  {
-    ssr: false,
-  }
-);
+  return str.slice(0, num) + '...';
+};
 
-export default function SearchPage() {
+const Roulette: FC = () => {
   const dispatch = useDispatch();
+  //   const entries = useSelector((state: RootState) => state.entry.entries);\
   const restaurants = useSelector(
     (state: RootState) => state.restaurant.restaurants
   );
-
-  const selectedPlace = useSelector(
-    (state: RootState) => state.map.selectedPlace
+  const selectedRestaurant = useSelector(
+    (state: RootState) => state.restaurant.selectedRestaurant
   );
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  const [mustSpin, setMustSpin] = useState(false);
+  // index of entry that wins the spin
 
-  useEffect(() => {
-    async function fetchRestaurants() {
-      setLoading(true);
+  // Color scheme
+  const colors = ['#250F47', '#FF035D', '#FF7F6C', '#980BFF'];
 
-      try {
-        const response = await fetch(
-          `/api/restaurants?lat=${selectedPlace?.lat}&lng=${selectedPlace?.lng}`
-        );
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data: any = await response.json();
+  const formattedData = restaurants.map((restaurant, index) => ({
+    option: truncateString(restaurant.name, 16), // Truncate the restaurant name to 20 characters
+    style: {
+      backgroundColor: colors[index % colors.length], // Distribute colors
+      textColor: '#ffffff',
+    }, // Optional styling
+  }));
 
-        // Dispatch the transformed data to Redux
-        dispatch(setRestaurants(data));
-      } catch (error) {
-        setError('Failed to fetch restaurants');
-        console.error('Failed to fetch restaurants', error);
-      } finally {
-        setLoading(false);
+  const handleSpin = () => {
+    if (!mustSpin) {
+      const newPrizeNumber = Math.floor(Math.random() * restaurants.length);
+
+      dispatch(setSelectedRestaurant(newPrizeNumber));
+      setMustSpin(true);
+    }
+  };
+
+  const handleStopSpinning = () => {
+    setMustSpin(false);
+    handleConfetti();
+    dispatch(setShowRestaurant(true));
+  };
+
+  const handleConfetti = () => {
+    const end = Date.now() + 2 * 1000; // Run for 2 seconds
+
+    // Random confetti particle creation function
+    const colors = ['#250F47', '#FF035D', '#FF7F6C', '#980BFF'];
+
+    (function frame() {
+      confetti({
+        particleCount: 5,
+        angle: 60,
+        spread: 55,
+        origin: { x: Math.random() },
+        colors: colors,
+      });
+      confetti({
+        particleCount: 5,
+        angle: 120,
+        spread: 55,
+        origin: { x: Math.random() },
+        colors: colors,
+      });
+
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
       }
-    }
-
-    if (selectedPlace?.lat && selectedPlace?.lng) {
-      fetchRestaurants();
-    }
-  }, [selectedPlace, selectedPlace?.lat, selectedPlace?.lng, dispatch]);
+    })();
+  };
 
   return (
-    <Container maxWidth="md" sx={{ textAlign: 'center', mt: 4 }}>
-      <Grid container spacing={2} alignItems="flex-start">
+    <Container maxWidth="lg">
+      <Grid container alignItems="center" justifyContent="center" spacing={2}>
         <Grid item xs={12}>
-          <SearchBar />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Wheel
+              mustStartSpinning={mustSpin}
+              prizeNumber={selectedRestaurant}
+              data={formattedData}
+              backgroundColors={['#3e3e3e', '#df3428']}
+              textColors={['#ffffff']}
+              onStopSpinning={handleStopSpinning}
+              fontSize={18}
+            />
+          </div>
         </Grid>
-        {restaurants.length === 0 && !loading ? (
-          <EmptyList />
-        ) : (
-          <Fragment>
-            {loading && (
-              <Grid item xs={12}>
-                <LinearBuffer />
-              </Grid>
-            )}
-            {!loading && (
-              <>
-                <Grid item xs={12} md={8}>
-                  {error ? (
-                    <Typography color="error">{error}</Typography>
-                  ) : (
-                    <Roulette />
-                  )}
-                </Grid>
-                <Grid item xs={12} md={4}>
-                  {error ? (
-                    <Typography color="error">{error}</Typography>
-                  ) : (
-                    <Restaurants restaurants={restaurants} />
-                  )}
-                </Grid>
-              </>
-            )}
-          </Fragment>
-        )}
+        <Grid item xs={12} textAlign="center">
+          <Button
+            onClick={handleSpin}
+            variant="contained"
+            color="secondary"
+            startIcon={<PlayArrowRoundedIcon />}
+            sx={{
+              padding: '10px 20px',
+            }}
+            disabled={mustSpin || restaurants.length < 2}
+          >
+            Spin the wheel
+          </Button>
+        </Grid>
       </Grid>
-      <SelectedRestaurant />
     </Container>
   );
-}
+};
+
+export default Roulette;
